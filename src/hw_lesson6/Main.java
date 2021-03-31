40f1b45f14709ebd8ad276802177282e3480db4e
grantkar <hrant87@inbox.ru>
2021-03-31 20:24:25 +0400

    lesson6

--- src/hw_lesson6/Main.java
+++ src/hw_lesson6/Main.java
@@ -1,7 +1,34 @@
 package hw_lesson6;
 
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Main {
     public static void main(String[] args) {
+
+        Random rd = new Random();
+
+        MyTreeMap<Integer, Integer> map = new MyTreeMap<>();
+
+        int countOfTrees = 200000;
+
+        int balanced = 0;
+        for (int i = 0; i < countOfTrees; i++) {
+            fillTreeMap(map);
+            if (map.isBalanced()) {
+                balanced++;
+            }
+        }
+        System.out.println("Из " + countOfTrees + " MyTreeMap сбалансированы " + balanced + "\r\nнесбалансированы " + (countOfTrees - balanced) + " деревьев");
+        System.out.println("Несбалансированы " + ((((float) (countOfTrees - balanced)) * 100) / (float) countOfTrees) + " %");
+    }
+
+
+    public static void fillTreeMap(MyTreeMap<Integer, Integer> map) {
+        while (map.getHeight() < 6) {
+            map.put(ThreadLocalRandom.current().nextInt(-100,100), 0);
+        }
+
 
     }
 }
--- src/hw_lesson6/MyTreeMap.java
+++ src/hw_lesson6/MyTreeMap.java
@@ -11,11 +11,13 @@ public class MyTreeMap <Key extends Comparable<Key>, Value>{
         Node left;
         Node right;
         int size;
+        int height;
 
         public Node(Key key, Value value) {
             this.key = key;
             this.value = value;
             size = 1;
+            height = 0;
         }
     }
 
@@ -85,6 +87,8 @@ public class MyTreeMap <Key extends Comparable<Key>, Value>{
             node.right = put(node.right, key, value);
         }
         node.size = size(node.left) + size(node.right) + 1;
+        updateHeight(node);
+
         return node;
     }
 
@@ -123,10 +127,11 @@ public class MyTreeMap <Key extends Comparable<Key>, Value>{
         }
         node.left = deleteMin(node.left);
         node.size = size(node.left) + size(node.right) + 1;
+        updateHeight(node);
         return node;
     }
 
-    public void delele(Key key) {
+    public void delete(Key key) {
         checkKeyNotNull(key);
         root = delete(root, key);
     }
@@ -153,8 +158,63 @@ public class MyTreeMap <Key extends Comparable<Key>, Value>{
             node.left = temp.left;
         }
         node.size = size(node.left) + size(node.right) + 1;
+        updateHeight(node);
         return node;
     }
+
+    public int getHeight (){
+        return height(root);
+    }
+
+    private int height(Node node) {
+        if (node == null){
+            return 0;
+        }
+        return node.height;
+    }
+
+    private int updateHeight (Node node){
+        if (node.left == null && node.right == null) {
+            return node.height = 0;
+        }
+        if (node.left != null && node.right == null) {
+           return node.height = node.left.height + 1;
+        }
+        if (node.left == null) {
+           return node.height = node.right.height + 1;
+        }
+        if (node.left.height > node.right.height) {
+           return node.height = node.left.height + 1;
+        }
+        if (node.left.height < node.right.height) {
+           return node.height = node.right.height + 1;
+        } else
+           return node.height = node.left.height + 1;
+
+    }
+
+    boolean isBalanced() {
+        if (root == null || root.size == 1) {
+            return true;
+        } else {
+            int leftHeight;
+            int rightHeight;
+
+            if (root.left == null) {
+                leftHeight = 0;
+            } else {
+                leftHeight = root.left.height;
+            }
+
+            if (root.right == null) {
+                rightHeight = 0;
+            } else {
+                rightHeight = root.right.height;
+            }
+            return Math.abs(leftHeight - rightHeight) <= 1;
+        }
+    }
+
 
     @Override
     public String toString() {
